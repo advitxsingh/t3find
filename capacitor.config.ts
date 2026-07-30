5d185a8b88a439c9474fd3d259022de646372aa5
Advit <advitsingh.4862@gmail.com>
2026-07-30 18:41:39 +0530

    android thing

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.t3find.app',
+  appName: 'T3Find',
+  webDir: 'dist'
+};
+
+export default config;
